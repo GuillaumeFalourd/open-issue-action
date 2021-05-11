7172d1321ac6d37f30aba7bed08935bd396c9910
GuillaumeFalourd <guillaume.falourd@zup.com.br>
2021-05-11 11:06:03 -0300

    update entrypoint.sh file
    Signed-off-by: GuillaumeFalourd <guillaume.falourd@zup.com.br>

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,5 @@ sudo rit set credential --provider=github --fields=token --values="$1"
 echo "🔄 Importing Github API formula repository"
 sudo rit add repo --provider=Github --name=formulas-github --repoUrl=https://github.com/GuillaumeFalourd/formulas-github --priority=0
 
-echo "⚙️  Start automation opening the ISSUE on $2's $3 repository"
+echo "⚙️  Running automation opening the ISSUE on $2's $3 repository"
 sudo rit github create issue --rit_github_owner="$2" --rit_github_repo="$3" --rit_issue_title="$4" --rit_issue_body="$5" --rit_issue_labels="$6" --rit_issue_assignees="$7"
